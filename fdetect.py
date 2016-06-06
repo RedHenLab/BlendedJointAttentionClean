5ac1e101d94ff69e85ca172ec0932303e3715b2e
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-07 00:34:24 +0530

    Add video capture

--- fdetect.py
+++ fdetect.py
@@ -1,3 +1,11 @@
 import cv2
 import sys
 
+def video_capture(height, width):
+	cam = cv2.VideoCapture(-1)
+	cam.set(3, width)
+	cam.set(4, heigth)
+	video_capture = cam
+	return video_capture.read()
+
+def face_detect_frontal(frame):
