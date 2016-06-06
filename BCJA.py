e2e587304d09cf69bfdd6b50c7ec4ddb5c95fd6b
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-06 23:53:04 +0530

    Add profile face cascade

--- BCJA.py
+++ BCJA.py
@@ -22,9 +22,8 @@ import fdetect
 import getcascades
 
 #Returns a 3 element array
-
 def get_frontal_face_cascade():
 	return getcascades.frontal_face()
 
-
-profile_face = getcascades.profile_face()
+def get_profile_face_cascade():
+	return getcascades.profile_face()
