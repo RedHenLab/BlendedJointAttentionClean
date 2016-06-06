eabd1f071f3c29ab3c37673f063daf97dc6dc1be
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-06 23:28:34 +0530

    Add description

--- BCJA.py
+++ BCJA.py
@@ -4,23 +4,19 @@
 #	[]	 []	[]		[] []   []	  []
 #	[][][]	 [][][]  [][]   []	  []
 #
+#	================================================
+# 	Library used to detect facial features, gestures
+#	and other thigs, finally used to detect and cla-
+#	-ssify different instances of blended classical
+#	joint attention. For further information and us-
+#	-age see ReadMe.md. 
+#	================================================
 #
-# Library used to detect facial features 
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+#	Liscence is hereby provided to everyone abiding
+#	by the liscecnces of the dependencies to use this
+#	library when and where needed without a need to 
+# 	state. 
+#	================================================
 
 import fdetect
 import getcascades
