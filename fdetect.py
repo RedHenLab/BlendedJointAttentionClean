da095a887ebfe9d275068697b01651a40f561906
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-07 01:09:26 +0530

    Correct path

--- fdetect.py
+++ fdetect.py
@@ -21,7 +21,7 @@ def webcam_face_detect_frontal(facecascade):
 		        for (x, y, w, h) in faces:
 		            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
 	        # Display the resulting frame
-	        cv2.imshow('Video', frame)
+	        # cv2.imshow('Video', frame)
 	        if cv2.waitKey(1) & 0xFF == ord('q'):
 	            break
 	# Release video capture
