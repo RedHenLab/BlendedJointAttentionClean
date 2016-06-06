3e0d21584d31d12fe2b148f560d3fa3b51ca0ccc
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-07 01:09:45 +0530

    Correct path

--- getcascades.py
+++ getcascades.py
@@ -2,16 +2,16 @@ import cv2
 
 
 def frontal_face():
-	faceCascade1 = cv2.CascadeClassifier('cascades/haarcascades/haarcascade_frontalface_alt2.xml')
-	faceCascade2 = cv2.CascadeClassifier('cascades/haarcascades/haarcascade_frontalface_default.xml')
-	faceCascade3 = cv2.CascadeClassifier('cascades/haarcascades/haarcascade_frontalface_alt.xml')
+	faceCascade1 = cv2.CascadeClassifier('Cascades/haarcascades/haarcascade_frontalface_alt2.xml')
+	faceCascade2 = cv2.CascadeClassifier('Cascades/haarcascades/haarcascade_frontalface_default.xml')
+	faceCascade3 = cv2.CascadeClassifier('Cascades/haarcascades/haarcascade_frontalface_alt.xml')
 	return [faceCascade1,faceCascade2,faceCascade3]
 
 def profile_face():
-	faceCascade1 = cv2.CascadeClassifier('cascades/haarcascades/haarcascade_profileface.xml')
-	faceCascade2 = cv2.CascadeClassifier('cascades/lbp_cascades/lbpcascade_profileface.xml')
+	faceCascade1 = cv2.CascadeClassifier('Cascades/haarcascades/haarcascade_profileface.xml')
+	faceCascade2 = cv2.CascadeClassifier('Cascades/lbp_cascades/lbpcascade_profileface.xml')
 	return [faceCascade1,faceCascade2]
 
 def facial_landmarks():
-	predictor = dlib.shape_predictor('cascades/dlibcascades/shape_predictor_68_face_landmarks.dat')
+	predictor = dlib.shape_predictor('Cascades/dlibcascades/shape_predictor_68_face_landmarks.dat')
 	return predictor
