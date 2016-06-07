d4d1291f2967580aaf10e52cdec9222afbd78eba
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-08 04:46:20 +0530

    Seperate single and multiple face detect

--- fdetect.py
+++ fdetect.py
@@ -9,7 +9,7 @@ def video_read(height, width):
 	video_capture = cam
 	return video_capture
 
-def webcam_face_detect_frontal(facecascade):
+def webcam_face_detect_frontal_single(facecascade):
 	video = video_read(480,640)
 	while True:
 		ret, frame = video.read()
@@ -32,4 +32,25 @@ def webcam_face_detect_frontal(facecascade):
 	video.release()
 	cv2.destroyAllWindows()
 
+def webcam_face_detect_frontal(facecascade):
+	video = video_read(480,640)
+	while True:
+		ret, frame = video.read()
+		if ret:
+			for i in range(len(facecascade)):
+				gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+				faces = facecascade[i].detectMultiScale(gray, 1.1, 5)
+		        # Draw a rectangle around the faces
+		        for (x, y, w, h) in faces:
+		            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+		            print("Face detected")
+	        # Display the resulting frame
+	        # print(type(frame))
+	        if frame.any()>0:
+	        	cv2.imshow('Video', frame)
+	        if cv2.waitKey(1) & 0xFF == ord('q'):
+	            break
+	# Release video capture
+	video.release()
+	cv2.destroyAllWindows()
 
