5762dcff19ae11f7df1009268332924866bd4a76
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-08 04:51:01 +0530

    Add commit

--- BCJA.py
+++ BCJA.py
@@ -33,18 +33,22 @@ def get_profile_face_cascade():
 def get_facial_landmarks():
 	return getcascades.facial_landmarks()
 
+# Takes input from webcam and detects a single frontal face out of many
 def get_webcam_frontal_face_single():
 	facecascade = get_frontal_face_cascade()
 	fdetect.webcam_face_detect_frontal_single(facecascade)
 
+# Takes input from webcam and detects all frontal faces
 def get_webcam_frontal_face():
 	facecascade = get_frontal_face_cascade()
 	fdetect.webcam_face_detect_frontal(facecascade)
 
+# Takes input from webcam and detects a single profile face out of many
 def get_webcam_profile_face_single():
 	facecascade = get_frontal_face_cascade()
 	fdetect.webcam_face_detect_profile_single(facecascade)
 
+# Takes input from webcam and detects all profile faces
 def get_webcam_profile_face():
 	facecascade = get_frontal_face_cascade()
 	fdetect.webcam_face_detect_profile(facecascade)
