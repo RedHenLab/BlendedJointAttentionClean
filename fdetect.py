d66853e824029033c4f3f59d183cdc8ee2f61f1e
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-08 05:52:27 +0530

    Correct indent

--- fdetect.py
+++ fdetect.py
@@ -67,20 +67,20 @@ def webcam_face_detect_template_matching(facecascade):
 		if ret:
 			if flag == 0:
 				gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	            for i in range(len(facecascade)):
-	            	faces1 = faceCascade[i].detectMultiScale(gray, 1.1, 5)
-		            # Draw a rectangle around the faces
-		            for (x, y, w, h) in faces1:
-		                template = gray[y:y+h, x:x+w]
-		                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-		                roi_gray = gray[(y-10):(y+h+10), (x-10):(x+w+10)]
-		                roi_gray2 = gray[(y-20):(y+h+20), (x-20):(x+w+20)]
-		                last_x = x
-		                last_y = y
-		                last_h = h
-		                last_w = w
-		                flag = 1
-		                break
+				for i in range(len(facecascade)):
+					faces1 = faceCascade[i].detectMultiScale(gray, 1.1, 5)
+					# Draw a rectangle around the faces
+					for (x, y, w, h) in faces1:
+					    template = gray[y:y+h, x:x+w]
+					    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+					    roi_gray = gray[(y-10):(y+h+10), (x-10):(x+w+10)]
+					    roi_gray2 = gray[(y-20):(y+h+20), (x-20):(x+w+20)]
+					    last_x = x
+					    last_y = y
+					    last_h = h
+					    last_w = w
+					    flag = 1
+					    break
 
 	        else :
 	            flag = 0
