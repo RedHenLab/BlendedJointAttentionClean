6a22204e16987f950e7956e3ad54ca6744588cd6
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-08 05:54:29 +0530

    Add new tests

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import BCJA
 # BCJA.get_webcam_frontal_face_single()
 # BCJA.get_webcam_profile_face_single()
 # BCJA.get_webcam_face_template()
-# BCJA.get_webcam__face_single()
+BCJA.get_webcam__face_single()
 # BCJA.get_webcam__face()
 # BCJA.get_webcam_face_template()
 
