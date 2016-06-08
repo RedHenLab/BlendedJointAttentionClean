45ffd8fa07880ea221b87c71e297871fcbf69df3
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-08 05:56:32 +0530

    Correct typo

--- BCJA.py
+++ BCJA.py
@@ -58,7 +58,7 @@ def get_webcam_profile_face():
 
 # Takes input from webcam and detects single face of all kinds
 def get_webcam__face_single():
-	acecascade = [get_profile_face_cascade(), get_frontal_face_cascade()]
+	facecascade = [get_profile_face_cascade(), get_frontal_face_cascade()]
 	fdetect.webcam_face_detect_single(facecascade)
 
 # Takes input from webcam and detects all face of all kinds
