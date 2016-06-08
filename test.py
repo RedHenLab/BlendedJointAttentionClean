4c32fb525874bf4aea73d15bb7eede4e91b8926e
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-08 06:10:07 +0530

    All cascade tests

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import BCJA
 
+# facecascade = BCJA.get_all_face_cascade()
+# print(len(facecascade))
+
 # BCJA.get_webcam_frontal_face()
 # BCJA.get_webcam_profile_face()
 # BCJA.get_webcam_frontal_face_single()
 # BCJA.get_webcam_profile_face_single()
 # BCJA.get_webcam_face_template()
-BCJA.get_webcam__face_single()
+# BCJA.get_webcam__face_single()
 # BCJA.get_webcam__face()
 # BCJA.get_webcam_face_template()
 
