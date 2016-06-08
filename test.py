cadb0aeb284823c23d6246a2de6d8f1cc68bf5f7
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-08 05:42:41 +0530

    Comment all tests

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 import BCJA
 
-BCJA.get_webcam_frontal_face_single()
+# BCJA.get_webcam_frontal_face()
+# BCJA.get_webcam_profile_face()
+# BCJA.get_webcam_frontal_face_single()
+# BCJA.get_webcam_profile_face_single()
+# BCJA.get_webcam_face_template()
+
