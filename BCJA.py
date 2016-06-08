d7e16d73be6c794f048f7f234e7a5f49b30d37c0
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-08 05:32:59 +0530

    Add profile face code

--- BCJA.py
+++ BCJA.py
@@ -48,11 +48,11 @@ def get_webcam_frontal_face():
 
 # Takes input from webcam and detects a single profile face out of many
 def get_webcam_profile_face_single():
-	facecascade = get_frontal_face_cascade()
-	fdetect.webcam_face_detect_profile_single(facecascade)
+	facecascade = get_profile_face_cascade()
+	fdetect.webcam_face_detect_single(facecascade)
 
 # Takes input from webcam and detects all profile faces
 def get_webcam_profile_face():
-	facecascade = get_frontal_face_cascade()
-	fdetect.webcam_face_detect_profile(facecascade)
+	facecascade = get_profile_face_cascade()
+	fdetect.webcam_face_detect(facecascade)
 
