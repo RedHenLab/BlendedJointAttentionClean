b04c9626a0254e8e4bfb1a574bbf3266a9ee7716
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-15 14:21:51 +0530

    Add mp4 test modules

--- test.py
+++ test.py
@@ -9,6 +9,6 @@ import BCJA
 # BCJA.get_webcam_profile_face_single()
 # BCJA.get_webcam_face_template()
 # BCJA.get_webcam__face_single()
-# BCJA.get_webcam__face()
+BCJA.get_webcam__face()
 # BCJA.get_webcam_face_template()
 
