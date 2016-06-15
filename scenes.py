222aea666debb047efc5f2e1e38ed7318644bcb5
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-15 14:25:42 +0530

    Import better code

--- scenes.py
+++ scenes.py
@@ -2,41 +2,34 @@ import cv2
 
 def scene_change(input_file):
 	cam = cv2.VideoCapture(input_file)
-	cam.set(3,640)
-	cam.set(4,480)
-	video_capture = cam
-	frame_num = 0
-	scene_num = 0
-	while True:
-	    # Capture frame-by-frame
-	    ret, frame = video_capture.read()
-		cv2.ocl.setUseOpenCL(False)
-		fgbg = cv2.createBackgroundSubtractorMOG2()
-		frame_num = 0
-		last_detected = -20
-		scene_num = 0
-		while(1):
-			frame_num = frame_num + 1
-			ret, frame = video_capture.read()
-			fgmask = fgbg.apply(frame)
-			num_white = 0
-			flag = 0
-			if(frame_num-last_detected>40):
-				last_detected = frame_num
-				for i in range(fgmask.shape[0]):
-					for j in range(fgmask.shape[1]):
-						if fgmask[i][j] == 255:
-							num_white = num_white+1
-							if(num_white>0.8*fgmask.shape[0]*fgmask.shape[0]):
-								scene_num = scene_num + 1
-								print("Scene changed : ", scene_num)
-								flag = 1
-								break
-					if flag == 1:
+	video_capture = cap
+cv2.ocl.setUseOpenCL(False)
+fgbg = cv2.createBackgroundSubtractorMOG2()
+frame_num = 0
+last_detected = -20
+scene_num = 0
+while(1):
+	frame_num = frame_num + 1
+	ret, frame = video_capture.read()
+	fgmask = fgbg.apply(frame)
+	num_white = 0
+	flag = 0
+	if(frame_num-last_detected>40):
+		last_detected = frame_num
+		for i in range(fgmask.shape[0]):
+			for j in range(fgmask.shape[1]):
+				if fgmask[i][j] == 255:
+					num_white = num_white+1
+					if(num_white>0.8*fgmask.shape[0]*fgmask.shape[0]):
+						scene_num = scene_num + 1
+						print("Scene changed : ", scene_num)
+						flag = 1
 						break
-			cv2.imshow('Video',fgmask)
-			k = cv2.waitKey(30) & 0xff
-			if k == 27:
+			if flag == 1:
 				break
-	cap.release()
-	cv2.destroyAllWindows()
+	cv2.imshow('Video',fgmask)
+	k = cv2.waitKey(30) & 0xff
+	if k == 27:
+		break
+cap.release()
+cv2.destroyAllWindows()
