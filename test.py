53cad2fbbb305e568ea93e891addd288ccc20217
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-24 16:16:28 +0530

    Check profile face

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import BCJA
 # facecascade = BCJA.get_all_face_cascade()
 # print(len(facecascade))
 
-BCJA.get_webcam_frontal_face()
-# BCJA.get_webcam_profile_face()
+# BCJA.get_webcam_frontal_face()
+BCJA.get_webcam_profile_face()
 # BCJA.get_webcam_frontal_face_single()
 # BCJA.get_webcam_profile_face_single()
 # BCJA.get_webcam_face_template()
