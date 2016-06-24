c48c45410dd2752aac12c87c091942b4b0380ac6
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-24 17:16:46 +0530

    Minor corrections'

--- gaze.py
+++ gaze.py
@@ -31,64 +31,65 @@ def cross_spread(split):
 	return centre
 
 # Video capture via webcam
-cam = cv2.VideoCapture("test.mp4")
-cam.set(3,640)
-cam.set(4,480)
-video_capture = cam
+def detect_gaze_direction(video_capture):
+	cam = cv2.VideoCapture(video_capture)
+	cam.set(3,640)
+	cam.set(4,480)
+	video_capture = cam
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('../dlibcascades/shape_predictor_68_face_landmarks.dat')
+	detector = dlib.get_frontal_face_detector()
+	predictor = dlib.shape_predictor('../dlibcascades/shape_predictor_68_face_landmarks.dat')
 
 
-while True:
-    # Capture frame-by-frame
-	ret, frame = video_capture.read()
-	if ret:
-		frame_color = frame
-		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		dets = detector(frame, 1)
-		for k, d in enumerate(dets):
-	        # Get the landmarks/parts for the face in box d.
-			shape = predictor(frame, d)
-	        # print(type(shape.part(1).x))
-			cv2.circle(frame_color,(shape.part(36).x,shape.part(36).y),2,(0,0,255))
-			cv2.circle(frame_color,(shape.part(39).x,shape.part(39).y),2,(0,0,255))
-			cv2.circle(frame_color,(shape.part(42).x,shape.part(42).y),2,(0,0,255))
-			cv2.circle(frame_color,(shape.part(45).x,shape.part(45).y),2,(0,0,255))
-			x1 = shape.part(36).x
-			y1 = shape.part(37).y-2
-			x2 = shape.part(39).x
-			y2 = shape.part(40).y+2
-			split = frame[y1:y2,x1:x2]
-			split = process_eye(split)
-			split = filter_eye(split)
-			centre = cross_spread(split)
-			frame[y1:y2,x1:x2]=split
-			y1 = y1+2
-			y2 = y2-2
-			centre[1]=centre[1]-2
-			# cv2.rectangle(frame_color,(x1,y1), (x2,y2), (0, 0, 255), 1)
-			# cv2.circle(frame_color,(x1+centre[0],y1+centre[1]),2,(0,0,255))
-			cv2.line(frame_color,(x1+centre[0],y1+centre[1]), (int((3*x1+4*centre[0]-x2)/2),int((3*y1+4*centre[1]-y2)/2)),(255,0,0))			
-			x1 = shape.part(42).x
-			y1 = shape.part(43).y-2
-			x2 = shape.part(45).x
-			y2 = shape.part(46).y+2
-			split = frame[y1:y2,x1:x2]
-			split = process_eye(split)
-			split = filter_eye(split)
-			centre = cross_spread(split)
-			frame[y1:y2,x1:x2]=split
-			y1 = y1+2
-			y2 = y2-2
-			centre[1]=centre[1]-2
-			# cv2.rectangle(frame_color,(x1,y1), (x2,y2), (0, 0, 255), 1)
-			# cv2.circle(frame_color,(x1+centre[0],y1+centre[1]),2,(0,0,255))
-			cv2.line(frame_color,(x1+centre[0],y1+centre[1]), (int((3*x1+4*centre[0]-x2)/2),int((3*y1+4*centre[1]-y2)/2)),(255,0,0))
-		# Display the resulting frame
-        cv2.imshow('Video', frame_color)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-# Release video capture
-video_capture.release()
-cv2.destroyAllWindows()
+	while True:
+	    # Capture frame-by-frame
+		ret, frame = video_capture.read()
+		if ret:
+			frame_color = frame
+			frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			dets = detector(frame, 1)
+			for k, d in enumerate(dets):
+		        # Get the landmarks/parts for the face in box d.
+				shape = predictor(frame, d)
+		        # print(type(shape.part(1).x))
+				cv2.circle(frame_color,(shape.part(36).x,shape.part(36).y),2,(0,0,255))
+				cv2.circle(frame_color,(shape.part(39).x,shape.part(39).y),2,(0,0,255))
+				cv2.circle(frame_color,(shape.part(42).x,shape.part(42).y),2,(0,0,255))
+				cv2.circle(frame_color,(shape.part(45).x,shape.part(45).y),2,(0,0,255))
+				x1 = shape.part(36).x
+				y1 = shape.part(37).y-2
+				x2 = shape.part(39).x
+				y2 = shape.part(40).y+2
+				split = frame[y1:y2,x1:x2]
+				split = process_eye(split)
+				split = filter_eye(split)
+				centre = cross_spread(split)
+				frame[y1:y2,x1:x2]=split
+				y1 = y1+2
+				y2 = y2-2
+				centre[1]=centre[1]-2
+				# cv2.rectangle(frame_color,(x1,y1), (x2,y2), (0, 0, 255), 1)
+				# cv2.circle(frame_color,(x1+centre[0],y1+centre[1]),2,(0,0,255))
+				cv2.line(frame_color,(x1+centre[0],y1+centre[1]), (int((3*x1+4*centre[0]-x2)/2),int((3*y1+4*centre[1]-y2)/2)),(255,0,0))			
+				x1 = shape.part(42).x
+				y1 = shape.part(43).y-2
+				x2 = shape.part(45).x
+				y2 = shape.part(46).y+2
+				split = frame[y1:y2,x1:x2]
+				split = process_eye(split)
+				split = filter_eye(split)
+				centre = cross_spread(split)
+				frame[y1:y2,x1:x2]=split
+				y1 = y1+2
+				y2 = y2-2
+				centre[1]=centre[1]-2
+				# cv2.rectangle(frame_color,(x1,y1), (x2,y2), (0, 0, 255), 1)
+				# cv2.circle(frame_color,(x1+centre[0],y1+centre[1]),2,(0,0,255))
+				cv2.line(frame_color,(x1+centre[0],y1+centre[1]), (int((3*x1+4*centre[0]-x2)/2),int((3*y1+4*centre[1]-y2)/2)),(255,0,0))
+			# Display the resulting frame
+	        cv2.imshow('Video', frame_color)
+	        if cv2.waitKey(1) & 0xFF == ord('q'):
+	            break
+	# Release video capture
+	video_capture.release()
+	cv2.destroyAllWindows()
