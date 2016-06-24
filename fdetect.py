f8f716ad856690444567fbf2c2092542d811c235
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-24 16:44:30 +0530

    Minor corrections

--- fdetect.py
+++ fdetect.py
@@ -59,58 +59,56 @@ def webcam_face_detect(facecascade):
 	cv2.destroyAllWindows()
 
 def webcam_face_detect_template_matching(facecascade):
+	video_capture = video_read(480,640)
 	frame_number = 0
 	flag = 0
 	last_x = 0
 	last_y = 0
 	last_w = 0
 	last_h = 0
-	video = video_read(480,640)
+
 	while True:
-		ret, frame = video.read()
-		if ret:
-			if flag == 0:
-				gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-				for i in range(len(facecascade)):
-					faces1 = facecascade[i].detectMultiScale(gray, 1.1, 5)
-					# Draw a rectangle around the faces
-					for (x, y, w, h) in faces1:
-						template = gray[y:y+h, x:x+w]
-						cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-						roi_gray = gray[(y-10):(y+h+10), (x-10):(x+w+10)]
-						roi_gray2 = gray[(y-20):(y+h+20), (x-20):(x+w+20)]
-						last_x = x
-						last_y = y
-						last_h = h
-						last_w = w
-						flag = 1
-						frame_number = frame_number+1
-						break
+	    # Capture frame-by-frame
+	    ret, frame = video_capture.read()
+	    if ret:
+	        if flag == 0:
+	            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	            faces1 = faceCascade1.detectMultiScale(gray, 1.1, 5)
+	            # Draw a rectangle around the faces
+	            for (x, y, w, h) in faces1:
+	                template = gray[y:y+h, x:x+w]
+	                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+	                roi_gray = gray[(y-10):(y+h+10), (x-10):(x+w+10)]
+	                roi_gray2 = gray[(y-20):(y+h+20), (x-20):(x+w+20)]
+	                last_x = x
+	                last_y = y
+	                last_h = h
+	                last_w = w
+	                flag = 1
+	                frame_number=frame_number+1
 
 	        else :
 	            flag = 0
 	            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	            for i in range(len(facecascade)):
-		            faces1 = faceCascade[i].detectMultiScale(roi_gray, 1.1, 5)
-		            
-		            # Draw a rectangle around the faces
-		            for (x, y, w, h) in faces1:
-		                x = last_x+x-10
-		                y = last_y+y-10
-		                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-		                roi_gray = gray[(y-10):(y+h+10), (x-10):(x+w+10)]
-		                roi_gray2 = gray[(y-20):(y+h+20), (x-20):(x+w+20)]
-		                template = gray[y:y+h, x:x+w]
-		                last_x = x
-		                last_y = y
-		                last_h = h
-		                last_w = w
-		                flag = 1
-		                frame_number=frame_number+1
-		                # break
+	            faces1 = faceCascade1.detectMultiScale(roi_gray, 1.1, 5)
+	            
+	            # Draw a rectangle around the faces
+	            for (x, y, w, h) in faces1:
+	                x = last_x+x-10
+	                y = last_y+y-10
+	                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+	                roi_gray = gray[(y-10):(y+h+10), (x-10):(x+w+10)]
+	                roi_gray2 = gray[(y-20):(y+h+20), (x-20):(x+w+20)]
+	                template = gray[y:y+h, x:x+w]
+	                last_x = x
+	                last_y = y
+	                last_h = h
+	                last_w = w
+	                flag = 1
+	                frame_number=frame_number+1
 
 	        if flag == 0 and frame_number != 0:
-	            
 	            # Apply template Matching
 	            res = cv2.matchTemplate(roi_gray2,template,cv2.TM_CCOEFF)
 	            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
@@ -120,14 +118,11 @@ def webcam_face_detect_template_matching(facecascade):
 	            cv2.rectangle(frame,top_left, bottom_right, (0,255,0), 2)
 	            roi_gray = gray[top_left[1]-10:top_left[1]+last_h+10, top_left[0]-10:(top_left[0]+last_w+10)]
 	            frame_number = frame_number + 1
-	        
 	        # Display the resulting frame
 	        cv2.imshow('Video', frame)
 	        if cv2.waitKey(1) & 0xFF == ord('q'):
 	            break
 	        print(frame_number)
 	# Release video capture
-	video.release()
+	video_capture.release()
 	cv2.destroyAllWindows()
-
-
