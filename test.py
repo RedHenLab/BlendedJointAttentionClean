596b1e870c5f939b36916c3b54a09e159a56d6c1
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-24 16:18:28 +0530

    Template matching test not working

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ import BCJA
 # print(len(facecascade))
 
 # BCJA.get_webcam_frontal_face()
-BCJA.get_webcam_profile_face()
+# BCJA.get_webcam_profile_face()
 # BCJA.get_webcam_frontal_face_single()
 # BCJA.get_webcam_profile_face_single()
-# BCJA.get_webcam_face_template()
+BCJA.get_webcam_face_template()
 # BCJA.get_webcam__face_single()
 # BCJA.get_webcam__face()
 # BCJA.get_webcam_face_template()
