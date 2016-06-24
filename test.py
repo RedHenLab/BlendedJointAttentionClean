ece3cba3fe7e1f89b79755f10b5a837bbc85e24b
Soumitra <agarwalsoumitra1504@gmail.com>
2016-06-24 16:12:15 +0530

    Face test

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import BCJA
 # facecascade = BCJA.get_all_face_cascade()
 # print(len(facecascade))
 
-# BCJA.get_webcam_frontal_face()
+BCJA.get_webcam_frontal_face()
 # BCJA.get_webcam_profile_face()
 # BCJA.get_webcam_frontal_face_single()
 # BCJA.get_webcam_profile_face_single()
@@ -11,4 +11,4 @@ import BCJA
 # BCJA.get_webcam__face_single()
 # BCJA.get_webcam__face()
 # BCJA.get_webcam_face_template()
-BCJA.scene_change('test.mp4')
+# BCJA.scene_change('test.mp4')
