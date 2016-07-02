aaf6162dc508a2b3b274d6880ba00bb54b7baf54
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-02 23:55:42 +0530

    Remove useless methods

--- head.py
+++ head.py
@@ -3,14 +3,6 @@ import numpy as np
 import dlib 
 import fdetect
 
-cam = cv2.VideoCapture(-1)
-cam.set(3,640)
-cam.set(4,480)
-video_capture = cam
-
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('../dlibcascades/shape_predictor_68_face_landmarks.dat')
-
 def face_pose(facecascade):
 	video = fdetect.video_read(480,640)
 	while True:
