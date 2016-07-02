2fe70b67931ec3b80007321eb849e8da9ef73e18
Soumitra <agarwalsoumitra1504@gmail.com>
2016-07-02 23:55:57 +0530

    Keep detector

--- head.py
+++ head.py
@@ -3,6 +3,9 @@ import numpy as np
 import dlib 
 import fdetect
 
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('../dlibcascades/shape_predictor_68_face_landmarks.dat')
+
 def face_pose(facecascade):
 	video = fdetect.video_read(480,640)
 	while True:
